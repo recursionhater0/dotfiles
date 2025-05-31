ef285b5a65dc64f6a8ba07f44898dfe4580f29a6
ramazan <r.rakhimbayev@mycar.kz>
2025-05-31 23:35:03 +0500

    Add macOS system defaults configuration script
    Includes personal preferences for UI behavior, Finder, Safari, Dock, menu bar, text editing, and security. Designed for repeatable setup  on new machines with minimal manual tweaks.

---
+++ macos/settings.sh
@@ -0,0 +1,171 @@
+#!/usr/bin/env bash
+
+set -e
+
+# This file contains settings for mac which makes me happy.
+# It is not a full list.
+#
+# The best resource of finding new settings for other users is:
+# https://www.defaults-write.com
+#
+# Some parts are taken from:
+# - https://github.com/rootbeersoup/dotfiles
+# - https://github.com/skwp/dotfiles
+#
+# All values are sorted inside their blocks: newest are on the top.
+#
+
+echo 'Configuring your mac. Hang tight.'
+osascript -e 'tell application "System Preferences" to quit'
+
+
+# === General ===
+
+# Hide remaining battery time; show percentage
+defaults write com.apple.menuextra.battery ShowPercent -string "YES"
+defaults write com.apple.menuextra.battery ShowTime -string "NO"
+
+# Disable startup noise:
+sudo nvram SystemAudioVolume=%01
+
+# Scrollbars visible when scrolling:
+defaults write NSGlobalDomain AppleShowScrollBars -string "WhenScrolling"
+
+# Require password immediately after sleep or screen saver begins
+defaults write com.apple.screensaver askForPassword -int 1
+defaults write com.apple.screensaver askForPasswordDelay -int 0
+
+
+# === Dock ===
+
+# Size:
+defaults write com.apple.dock tilesize -int 56
+
+# Show indicator lights for open apps in Dock:
+defaults write com.apple.dock show-process-indicators -bool true
+
+# Dock size and location:
+defaults write com.apple.Dock size-immutable -bool yes
+
+# === Menu bar ===
+
+# Make status icons smaller, so they will take less space:
+# https://flaky.build/built-in-workaround-for-applications-hiding-under-the-macbook-pro-notch
+defaults write -globalDomain NSStatusItemSelectionPadding -int 12
+defaults write -globalDomain NSStatusItemSpacing -int 12
+
+
+# === Finder ===
+
+# Keep folders on top when sorting by name:
+defaults write com.apple.finder _FXSortFoldersFirst -bool true
+
+# Show Finder path bar:
+defaults write com.apple.finder ShowPathbar -bool true
+
+# Do not show status bar in Finder:
+defaults write com.apple.finder ShowStatusBar -bool false
+
+# Show hidden files in Finder:
+defaults write com.apple.finder AppleShowAllFiles -bool true
+
+# Show file extensions in Finder:
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+# Allow quitting Finder via ⌘ + Q; doing so will also hide desktop icons
+defaults write com.apple.finder QuitMenuItem -bool true
+
+# Allow text selection in Quick Look
+defaults write com.apple.finder QLEnableTextSelection -bool true
+
+# Display full POSIX path as Finder window title
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
+
+# Disable the warning when changing a file extension
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+
+# Show Library folder
+chflags nohidden ~/Library
+
+# When performing a search, search the current folder by default
+defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
+
+# Avoid creating .DS_Store files on network or USB volumes
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
+
+# Expand the following File Info panes:
+# “General”, “Open with”, and “Sharing & Permissions”
+defaults write com.apple.finder FXInfoPanesExpanded -dict \
+	General -bool true \
+	OpenWith -bool true \
+	Privileges -bool true
+
+
+# === Safari ===
+
+# Privacy: don’t send search queries to Apple
+defaults write com.apple.Safari UniversalSearchEnabled -bool false
+defaults write com.apple.Safari SuppressSearchSuggestions -bool true
+
+# Improve Safari security
+defaults write com.apple.Safari \
+  com.apple.Safari.ContentPageGroupIdentifier.WebKit2JavaEnabled \
+  -bool false
+defaults write com.apple.Safari \
+  com.apple.Safari.ContentPageGroupIdentifier.WebKit2JavaEnabledForLocalFiles \
+  -bool false
+
+
+# === Text editing ===
+
+# Disable smart quotes:
+defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
+
+# Disable autocorrect:
+defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
+
+# Disable auto-capitalization:
+defaults write NSGlobalDomain NSAutomaticCapitalizationEnabled -bool false
+
+# Disable smart dashes:
+defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
+
+# Diable automatic period substitution:
+defaults write NSGlobalDomain NSAutomaticPeriodSubstitutionEnabled -bool false
+
+
+# === Time Machine ===
+
+# Prevent Time Machine from prompting to use new hard drives as backup volume
+defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
+
+
+# === Activity monitor ===
+# Currently disabled for research purposes. I'm not sure yet whether I need these settings.
+
+# Show the main window when launching Activity Monitor
+#defaults write com.apple.ActivityMonitor OpenMainWindow -bool true
+
+# Visualize CPU usage in the Activity Monitor Dock icon
+#defaults write com.apple.ActivityMonitor IconType -int 5
+
+# Show all processes in Activity Monitor
+#defaults write com.apple.ActivityMonitor ShowCategory -int 0
+
+
+# === App Store ===
+
+# Disable in-app rating requests from apps downloaded from the App Store.
+defaults write com.apple.appstore InAppReviewEnabled -int 0
+
+
+# === Maccy ===
+
+
+# Restarting apps:
+echo 'Restarting apps...'
+killall Finder
+killall Dock
+
+echo 'Done!'
