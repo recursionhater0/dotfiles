44790063c700bcbdb433f55a6fd6717664bd227e
ramazan <rakhimbayev.ramazan@gmail.com>
2025-06-02 21:27:57 +0500

    Add hyper style

---
+++ config/hyper.js
@@ -0,0 +1,93 @@
+// Future versions of Hyper may add additional config options,
+// which will not automatically be merged into this file.
+// See https://hyper.is#cfg for all currently supported options.
+
+module.exports = {
+  config: {
+    // Choose either 'stable' for receiving highly polished,
+    // or 'canary' for less polished but more frequent updates
+    updateChannel: 'stable',
+
+    // default font size in pixels for all tabs
+    fontSize: 12,
+
+    // font family with optional fallbacks
+    fontFamily: 'Menlo, monospace',
+
+    // dark theme for testing:
+    // foregroundColor: '#fff',
+    // backgroundColor: '#0A0E14',
+    // borderColor: '#0A0E14',
+
+    // custom css to embed in the main window
+    css: '',
+
+    // custom css to embed in the terminal window
+    termCSS: '',
+
+    // the full list. if you're going to provide the full color palette,
+    // including the 6 x 6 color cubes and the grayscale map, just provide
+    // an array here instead of a color map object
+    colors: {},
+
+    // the shell to run when spawning a new session (i.e. /usr/local/bin/fish)
+    // if left empty, your system's login shell will be used by default
+    //
+    // Windows
+    // - Make sure to use a full path if the binary name doesn't work
+    // - Remove `--login` in shellArgs
+    //
+    // Bash on Windows
+    // - Example: `C:\\Windows\\System32\\bash.exe`
+    //
+    // Powershell on Windows
+    // - Example: `C:\\WINDOWS\\System32\\WindowsPowerShell\\v1.0\\powershell.exe`
+    shell: '',
+
+    // for setting shell arguments (i.e. for using interactive shellArgs: ['-i'])
+    // by default ['--login'] will be used
+    shellArgs: ['--login'],
+
+    // for environment variables
+    env: {
+      'TERM': 'xterm-256color',
+    },
+
+    // set to false for no bell
+    bell: false,
+
+    // if true, selected text will automatically be copied to the clipboard
+    copyOnSelect: false,
+
+    // number of lines to scroll upwards
+    // https://github.com/zeit/hyper/issues/280
+    scrollback: 10000,
+
+    // if true, on right click selected text will be copied or pasted if no
+    // selection is present (true by default on Windows)
+    // quickEdit: true
+
+    // URL to custom bell
+    // bellSoundURL: 'http://example.com/bell.mp3',
+
+    // for advanced config flags please refer to https://hyper.is/#cfg
+
+    // Change how terminal is opened:
+    summon: {
+      hotkey: 'ctrl+;'
+    }
+  },
+
+  // a list of plugins to fetch and install from npm
+  plugins: ['hyperterm-summon', 'hyper-search', "senya"],
+
+  // in development, you can create a directory under
+  // `~/.hyper_plugins/local/` and include it here
+  // to load it and avoid it being `npm install`ed
+  localPlugins: [],
+
+  keymaps: {
+    // Example
+    // 'window:devtools': 'cmd+alt+o',
+  }
+};
